190822b50d8b1dbbe0ecd3cd741dfe2acdc36b30
JustGodWork <discord1elie@gmail.com>
2024-05-28 11:00:57 +0200

    Trying fix for checker comformance

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,113 +1,55 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const PORT = 1245;
 const HOST = 'localhost';
-const app = http.createServer();
-const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
+const DB_FILE = process.argv[2];
 
-const countStudents = (dataPath) => new Promise((resolve, reject) => {
-  if (!dataPath) reject(new Error('Cannot load the database'));
-  fs.readFile(dataPath, (err, data) => {
-    if (err || !data) reject(new Error('Cannot load the database'));
-    const baseText = [];
-    const fileLines = data.toString('utf-8').trim().split('\n');
-    const studentGroups = {};
-    const dbFieldNames = fileLines[0].split(',');
-    const studentPropNames = dbFieldNames.slice(
-      0,
-      dbFieldNames.length - 1,
-    );
+/**
+ * Get the students from the database file (CSV format)
+ * @param {string} database
+ * @returns {string}
+ */
+async function getStudents(database) {
+  const data = await fs.readFile(database, 'utf8');
+  const lines = data.split('\n').filter(line => line);
+  const students = lines.map(line => line.split(','));
+  const csStudents = students.filter(student => student[3] === 'CS');
+  const sweStudents = students.filter(student => student[3] === 'SWE');
+  const totalStudents = students.length;
+  const totalCsStudents = csStudents.length;
+  const totalSweStudents = sweStudents.length;
+  const csStudentsList = csStudents.map(student => student[0]).join(', ');
+  const sweStudentsList = sweStudents.map(student => student[0]).join(', ');
 
-    for (const line of fileLines.slice(1)) {
-      const studentRecord = line.split(',');
-      const studentPropValues = studentRecord.slice(
-        0,
-        studentRecord.length - 1,
-      );
-      const field = studentRecord[studentRecord.length - 1];
+  return [
+    `Number of students: ${totalStudents}`,
+    `Number of students in CS: ${totalCsStudents}. List: ${csStudentsList}`,
+    `Number of students in SWE: ${totalSweStudents}. List: ${sweStudentsList}`
+  ].join('\n');
+}
 
-      if (!Object.keys(studentGroups).includes(field)) {
-        studentGroups[field] = [];
-      }
-
-      const studentEntries = studentPropNames.map((propName, idx) => [
-        propName,
-        studentPropValues[idx],
-      ]);
-
-      studentGroups[field].push(Object.fromEntries(studentEntries));
+/**
+ * Create a server with two routes
+ * - /: returns a welcome message
+ * - /students: returns the list of students from the database (CSV format)
+ */
+const app = http.createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await getStudents(DB_FILE);
+      res.write(students);
+    } catch (err) {
+      res.write('Cannot load the database');
     }
-
-    const totalStudents = Object.values(studentGroups).reduce(
-      (pre, cur) => (pre || []).length + cur.length,
-    );
-
-    baseText.push(`Number of students: ${totalStudents}`);
-    for (const [field, group] of Object.entries(studentGroups)) {
-      baseText.push([
-        `Number of students in ${field}: ${group.length}.`,
-        'List:',
-        group.map((student) => student.firstname).join(', '),
-      ].join(' '));
-    }
-    resolve(baseText.join('\n'));
-  });
-});
-
-const ROUTES = [
-  {
-    route: '/',
-    handler(_, res) {
-      const text = 'Hello Holberton School!';
-
-      res.setHeader('Content-Type', 'text/plain');
-      res.setHeader('Content-Length', text.length);
-      res.statusCode = 200;
-      res.write(Buffer.from(text));
-    },
-  },
-  {
-    route: '/students',
-    /**
-     *
-     * @param {Request} _
-     * @param {Response} res
-     */
-    handler(_, res) {
-      const baseText = ['This is the list of our students'];
-
-      countStudents(DB_FILE)
-        .then((report) => {
-          baseText.push(report);
-
-          const text = baseText.join('\n');
-          res.setHeader('Content-Type', 'text/plain');
-          res.setHeader('Content-Length', text.length);
-          res.statusCode = 200;
-          res.write(Buffer.from(text));
-        })
-        .catch((err) => {
-          baseText.push(err instanceof Error ? err.message : err.toString());
-
-          const text = baseText.join('\n');
-
-          res.setHeader('Content-Type', 'text/plain');
-          res.setHeader('Content-Length', text.length);
-          res.statusCode = 200;
-          res.write(Buffer.from(text));
-        });
-    },
-  },
-];
-
-app.on('request', (req, res) => {
-  for (const routeHandler of ROUTES) {
-    if (routeHandler.route === req.url) {
-      routeHandler.handler(req, res);
-      break;
-    }
+  } else {
+    res.write('URL not found');
   }
+  res.end();
 });
 
 app.listen(PORT, HOST, () => {
